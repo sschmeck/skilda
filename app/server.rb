79602627dd4c97901ecfe7e1689f1c0f86b75e6c
Matthias Baumgart <matthias@briefhansa.de>
2014-04-23 19:02:33 +0200

    Refactoring: Moved views to folder per resource.

--- app/server.rb
+++ app/server.rb
@@ -17,15 +17,15 @@ get '/' do
 end
 
 get '/skills' do
-  erb :skill_list, locals: { skills: Skill.all }
+  erb :"skill/list", locals: { skills: Skill.all }
 end
 
 get '/skills/:id' do |id|
-  erb :skill_detail, :locals => { :skill => Skill.find(id) }
+  erb :"skill/detail", :locals => { :skill => Skill.find(id) }
 end
 
 get '/persons' do
-  erb :person_list, locals: { persons: Person.all }
+  erb :"person/list", locals: { persons: Person.all }
 end
 
 post '/persons' do
@@ -33,11 +33,11 @@ post '/persons' do
   lastname = params['lastname']
   puts "create person: #{firstname} #{lastname}"
   person = Person.create!(firstname:firstname, lastname:lastname)
-  erb :person_detail, :locals => { :person => person, :skills => Skill.all, :levels => SKILL_LEVELS  }
+  erb :"person/detail", :locals => { :person => person, :skills => Skill.all, :levels => SKILL_LEVELS  }
 end
 
 get '/persons/:id' do |id|
-  erb :person_detail, :locals => { :person => Person.find(id), :skills => Skill.all, :levels => SKILL_LEVELS }
+  erb :"person/detail", :locals => { :person => Person.find(id), :skills => Skill.all, :levels => SKILL_LEVELS }
 end
 
 post '/persons/:id' do |id|
@@ -47,7 +47,7 @@ post '/persons/:id' do |id|
   
   person.skills.create(Skill.find(skill_id), :level => level)
   
-  erb :person_detail, :locals => { :person => Person.find(id), :skills => Skill.all, :levels => SKILL_LEVELS }
+  erb :"person/detail", :locals => { :person => Person.find(id), :skills => Skill.all, :levels => SKILL_LEVELS }
 end
 
 get '/skillcategories' do
